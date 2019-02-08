b5fa6cd0141e922d4eb4be31211eb7f6dd4fa220
Krzysztof Radecki <radeckikrzysztof@gmail.com>
2019-02-08 21:57:35 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,42 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            List<double> grades = new List<double>();
+            foreach (var s in Students)
+                grades.Add(s.AverageGrade);
+
+            grades.Sort();
+
+            int perGrade = (int)(Students.Count * 0.2d);
+            int mark = 5;
+
+            for (var i = Students.Count - 1 - perGrade; i >= 0; i -= perGrade)
+            {
+                if (averageGrade > grades[i])
+                {
+                    break;
+                }
+                else
+                {
+                    --mark;
+                }
+            }
+
+            switch(mark)
+            {
+                case 5: return 'A';
+                case 4: return 'B';
+                case 3: return 'C';
+                case 2: return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
