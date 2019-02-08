934b3223a9b4fcfe90b92eb41e36e9cac2fa072f
Krzysztof Radecki <radeckikrzysztof@gmail.com>
2019-02-08 22:04:59 +0100

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -38,7 +38,7 @@ namespace GradeBook.GradeBooks
                 }
             }
 
-            switch(mark)
+            switch (mark)
             {
                 case 5: return 'A';
                 case 4: return 'B';
@@ -47,6 +47,22 @@ namespace GradeBook.GradeBooks
             }
 
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
